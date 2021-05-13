3fd1863e45ebaccfa0d51213e1901eefd5fc2860
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-05-13 22:17:38 +0530

    changed 1st line

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ public class Test {
    
     public static void main(String[] args) {
         
-	System.out.println("Hello");
+	System.out.println("Hello Changed");
 	System.out.println("Hello By dinuka");
 
 	System.out.println("Hello world!");
