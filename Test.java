1ebbabf80855026cb44896277a5157f708ffb263
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-05-13 22:31:15 +0530

    Dinuka

--- Test.java
+++ Test.java
@@ -5,13 +5,14 @@ public class Test {
         
 	System.out.println("Hello 1");
 	System.out.println("Hello 2");
-
-        System.out.println("Hello 3");
-
+	System.out.println("Hello 3");
 	System.out.println("Hello 4");
-
 	System.out.println("Hello 5");
 	System.out.println("Hello 6");
+
+	for(int x=0,x<10;x++){
+		System.out.println("Hello"+x);
+	}
 
 
     }
