7c58db3504707613bd57afdc1c2ebc6fb54b03bc
Ridmi Dissanyake <ridmianjula@gmail.com>
2021-05-14 01:06:57 +0530

    Committed by ridmi

--- Test.java
+++ Test.java
@@ -1,28 +1,15 @@
 public class Test {
 
-   
+    static void hiEarth(){
+    	System.out.println("Hi earth !");
+    }
+    
     public static void main(String[] args) {
-        
-
-	System.out.println("Hello");
-	System.out.println("Hello By dinuka");
-
-        System.out.println("Hello world by ridmi");
-
-	System.out.println("Hello world!");
 
 	System.out.println("Hello world!");
 	System.out.println("Hello Srilanka! Stay Home ! Stay Safe !");
-
-	System.out.println("Hello 1");
-	System.out.println("Hello 2");
-	System.out.println("Hello 3");
-	System.out.println("Hello 4");
-	System.out.println("Hello 5");
-	System.out.println("Hello 6");
-
-        System.out.println("Hello Dinuka!");
+        
+	Test.hiEarth();
 
     }
-    
 }
