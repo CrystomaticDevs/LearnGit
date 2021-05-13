8035af69a05aef81f9e4e89ed21131edd220584b
Dinuka Dilshan <73058516+Dinuka-Dilshan@users.noreply.github.com>
2021-05-13 22:21:19 +0530

    changed

--- Test.java
+++ Test.java
@@ -3,15 +3,15 @@ public class Test {
    
     public static void main(String[] args) {
         
-	System.out.println("Hello Changed");
-	System.out.println("Hello By dinuka");
+	System.out.println("Hello 1");
+	System.out.println("Hello 2");
 
-        System.out.println("Hello world by ridmi");
+        System.out.println("Hello 3");
 
-	System.out.println("Hello world!");
+	System.out.println("Hello 4");
 
-	System.out.println("Hello world!");
-	System.out.println("Hello Srilanka! Stay Safe !");
+	System.out.println("Hello 5");
+	System.out.println("Hello 6");
 
 
     }
