141b6210fb18ba28cda5bfc69046072435eed34c
Sasindi <isuperera24@gmail.com>
2021-05-14 15:07:38 +0530

    name changes as sasindi 2

--- group.java
+++ group.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("my name is sasindi");
-	System.out.println("my name is Kalana");
+	System.out.println("my name is sasindi 2");
 	System.out.println("my name is Kalana");
     }
 }
