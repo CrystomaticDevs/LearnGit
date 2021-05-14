ede4af647e3f21f58db0a542d5f41b77efea22a6
kaushalyaweerakoon <kaushalya.2019272@iit.ac.lk>
2021-05-14 14:59:08 +0530

    change A to B

--- new.java
+++ new.java
@@ -1,6 +1,6 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("Group_A");
+		System.out.println("Group_B");
 		System.out.println("Group_A changed by Chithara");
 		System.out.println("Group_A 2nd change by Chithara");
 
