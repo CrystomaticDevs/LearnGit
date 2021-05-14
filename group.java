db95f6ec6955015d99e4d2c4a9560ae2202e988b
KalanaKariyawasam <kalana.2019353@iit.ac.lk>
2021-05-14 14:50:21 +0530

    Final edit

--- group.java
+++ group.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("my name is sasindi");
-	System.out.println("my name is Shazan");
+	System.out.println("my name is Kalana");
 	System.out.println("my name is Kalana");
     }
 }
